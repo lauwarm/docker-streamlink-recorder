7aa9458f238cc4e287e869b4b330cdaf416ab417
lauwarm <10065855+lauwarm@users.noreply.github.com>
2020-02-06 13:14:10 +0100

    changed date format

--- streamlink-recorder.sh
+++ streamlink-recorder.sh
@@ -3,7 +3,7 @@
 # For more information visit: https://github.com/downthecrop/TwitchVOD
 
 while [ true ]; do
-	Date=$(date +%F-%H%M%S)
+	Date=$(date +%Y%m%d-%H%M%S)
 	streamlink $streamOptions $streamLink $streamQuality -o /home/download/$streamName"-$Date".mkv
 	sleep 60s
 done
