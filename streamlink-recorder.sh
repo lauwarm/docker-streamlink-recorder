a45ad8c8adccb67ae0c1c4ba030e2aadb586beda
Adriel Kloppenburg <adriel@users.noreply.github.com>
2022-03-07 19:37:00 +1300

    Added kill option

--- streamlink-recorder.sh
+++ streamlink-recorder.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 while [[ true ]]; do
+  trap "echo Exited!; exit;" SIGINT SIGTERM # allow you to ctl+c while inside a whileloop
   streamlink --output "/home/download/{author}/{game}/time:%Y-%m-%d_%H.%M.%S} {title}.mkv" "$streamOptions" "$streamLink" "$streamQuality"
 	sleep 60s
 done
