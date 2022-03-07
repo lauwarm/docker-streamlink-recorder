ef5b9b45ad9aaecedacfad3e4644ce3cff707a27
Adriel Kloppenburg <adriel@users.noreply.github.com>
2022-03-07 20:32:45 +1300

    Update streamlink-recorder.sh

--- streamlink-recorder.sh
+++ streamlink-recorder.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 while [[ true ]]; do
-  trap "echo Exited!; exit;" SIGINT SIGTERM # allow you to ctl+c while inside a whileloop
+
+  echo "Starting streamlink streamOptions $streamOptions streamLink: $streamLink streamQuality: $streamQuality"
   streamlink --output "/home/download/{author}/{game}/{time:%Y-%m-%d_%H.%M.%S} {title}.mkv" "$streamOptions" "$streamLink" "$streamQuality"
 	sleep 60s
 done
