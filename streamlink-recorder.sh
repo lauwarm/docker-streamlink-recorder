79d87d7dbeb274ec8d6832be098f3962da8b8798
Adriel Kloppenburg <adriel@users.noreply.github.com>
2022-03-07 19:41:01 +1300

    Missing {

--- streamlink-recorder.sh
+++ streamlink-recorder.sh
@@ -2,6 +2,6 @@
 
 while [[ true ]]; do
   trap "echo Exited!; exit;" SIGINT SIGTERM # allow you to ctl+c while inside a whileloop
-  streamlink --output "/home/download/{author}/{game}/time:%Y-%m-%d_%H.%M.%S} {title}.mkv" "$streamOptions" "$streamLink" "$streamQuality"
+  streamlink --output "/home/download/{author}/{game}/{time:%Y-%m-%d_%H.%M.%S} {title}.mkv" "$streamOptions" "$streamLink" "$streamQuality"
 	sleep 60s
 done
