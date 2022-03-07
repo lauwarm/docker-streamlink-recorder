3fd346c1935ab0d0300debf22a0d4e41e3c40129
Adriel Kloppenburg <adriel@users.noreply.github.com>
2022-03-07 20:41:18 +1300

    Update streamlink-recorder.sh

--- streamlink-recorder.sh
+++ streamlink-recorder.sh
@@ -2,7 +2,7 @@
 
 while [[ true ]]; do
 
-  echo "Starting streamlink streamOptions $streamOptions streamLink: $streamLink streamQuality: $streamQuality"
+  echo "streamlink --output \"/home/download/{author}/{game}/{time:%Y-%m-%d_%H.%M.%S} {title}.mkv\" \"$streamOptions\" \"$streamLink\" \"$streamQuality\""
   streamlink --output "/home/download/{author}/{game}/{time:%Y-%m-%d_%H.%M.%S} {title}.mkv" "$streamOptions" "$streamLink" "$streamQuality"
 	sleep 60s
 done
