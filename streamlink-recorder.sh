dd704e40395b6ccca93fdaad2a2afb87bec6776f
lauwarm <10065855+lauwarm@users.noreply.github.com>
2024-04-12 13:33:37 +0200

    update Dockerfile to use /bin/bash and update script to parse streamOptions as an Array

--- streamlink-recorder.sh
+++ streamlink-recorder.sh
@@ -1,11 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
 # For more information visit: https://github.com/downthecrop/TwitchVOD
 
-IFS=';' read -r -a args <<< $streamOptions
+#echo "Argument 1: $streamOptions"
+#echo "Argument 2: $streamLink"
+#echo "Argument 3: $streamQuality"
+#echo "Argument 4: $streamName"
+
+IFS=';' read -r -a args <<< "$streamOptions"
 
 while [ true ]; do
 	Date=$(date +%Y%m%d-%H%M%S)
-	streamlink ${args[@]} $streamLink $streamQuality -o /home/download/$streamName"-$Date".mkv
+	streamlink "${args[@]}" "$streamLink" "$streamQuality" -o /home/download/"$streamName"-"$Date".mkv
 	sleep 60s
 done
