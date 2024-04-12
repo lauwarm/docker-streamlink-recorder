05260180689a4e3d62e96bcb526c884e144ca378
Fabian <10065855+lauwarm@users.noreply.github.com>
2024-04-12 13:19:11 +0200

    Update streamlink-recorder.sh

--- streamlink-recorder.sh
+++ streamlink-recorder.sh
@@ -2,10 +2,10 @@
 
 # For more information visit: https://github.com/downthecrop/TwitchVOD
 
-IFS=';' read -r -a args <<< "$streamOptions"
+IFS=';' read -r -a args <<< $streamOptions
 
 while [ true ]; do
 	Date=$(date +%Y%m%d-%H%M%S)
-	streamlink "${args[@]}" "$streamLink" "$streamQuality" -o /home/download/"$streamName"-"$Date".mkv
+	streamlink ${args[@]} $streamLink $streamQuality -o /home/download/$streamName"-$Date".mkv
 	sleep 60s
 done
